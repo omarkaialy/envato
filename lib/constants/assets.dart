919c41dda624fcee4fe9aa7eefb82c99c5649c73
Omar Kaialy <86903665+omarkaialy@users.noreply.github.com>
2023-11-05 17:58:31 +0200

    add splash screen

---
+++ lib/constants/assets.dart
@@ -0,0 +1,15 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsFontsInterBold
+  /// assets/fonts/Inter-Bold.ttf
+  static const String assetsFontsInterBold = "assets/fonts/Inter-Bold.ttf";
+
+  /// Assets for assetsFontsInterRegular
+  /// assets/fonts/Inter-Regular.ttf
+  static const String assetsFontsInterRegular = "assets/fonts/Inter-Regular.ttf";
+
+  /// Assets for assetsImagesLogo
+  /// assets/images/logo.png
+  static const String assetsImagesLogo = "assets/images/logo.png";
+}
---
+++ lib/splash/splash_screen.dart
@@ -0,0 +1,24 @@
+import 'package:envato/constants/assets.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Image.asset(
+          Assets.assetsImagesLogo,
+          width: .5 * MediaQuery.sizeOf(context).width,
+          fit: BoxFit.fitWidth,
+        ),
+      ),
+    );
+  }
+}
