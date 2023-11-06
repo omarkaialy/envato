8ca831e045a562b19279354ef7c3c5d8184ca25b
Halakadour <149234423+Halakadour@users.noreply.github.com>
2023-11-06 21:24:38 +0200

    udated creart acount screen

---
+++ lib/auth/creat_account_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CreateAcountScreen extends StatelessWidget {
+  const CreateAcountScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
