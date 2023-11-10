6348486b6ff3a282c85e3a038a58e17d7020329b
Mulham Ajam <molham2003@gmail.com>
2023-11-10 19:27:26 +0100

    Settings Screen Code

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:envato/auth/sign_in_screen.dart';
+import 'package:envato/settings/settings.dart';
 //import 'package:envato/splash/splash_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -12,7 +13,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: SignInScreen(),
+      home: SETTINGS(),
       //SplashScreen(),
       debugShowCheckedModeBanner: false,
     );
