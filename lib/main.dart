7628e67a3df5d34e2d8ba077e16720a7465632c1
Omar Kaialy <86903665+omarkaialy@users.noreply.github.com>
2023-11-11 18:24:24 +0200

    edit main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
+import 'package:envato/splash/splash_screen.dart';
 import 'package:flutter/material.dart';
-
-import 'onboard/onboard_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -12,7 +11,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Onboard(),
+      home: SplashScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
